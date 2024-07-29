aa6483378a2fb16f8acd78b920d4c6560f736d08
Buumty <wojtekand22@gmail.com>
2024-07-29 21:15:22 +0200

    Initial commit

---
+++ to-do-rest/src/test/java/com/wojciechandrzejczak/to_do_rest/ToDoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojciechandrzejczak.to_do_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
